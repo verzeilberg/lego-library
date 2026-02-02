83abf879d02d037f4c857638e99a83b608542a07
Sander Verzeilberg <verzeilberg@srs.nl>
2026-02-02 13:50:53 +0100

    Added homepage

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('index/index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
